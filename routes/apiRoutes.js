9125f9843cdf78bbc0208c8bc17d583ec504a18c
Bailey Clemons <baileythevampireslayer@gmail.com>
2021-01-06 20:07:53 -0800

    "finished delete route"

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,42 +2,70 @@
 const { query } = require("express");
 let fs = require("fs");
 
+
 module.exports = function (app) {
 
-  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
 
   //Read File
   app.get("/api/notes", (req, res) => {
 
-    res.json(notes);
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
 
   });
+
 
   //Post File
   app.post("/api/notes", (req, res) => {
 
-    let newNote = req.body;
-    newNote.id = notes.length + 1;
-    notes.push(newNote),
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
 
-      fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), (err) => {
-        if (err) {
-          console.log(err);
-        }
+      let newNote = req.body;
+      newNote.id = notes.length + 1;
+      notes.push(newNote);
 
-      });
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+          if (err) {
+            console.log(err);
+          }
+          res.json({ ok: true });
+        });
 
-    res.json({ ok: true });
+    });
 
   });
 
 
+  
   //Delete File
   app.delete("/api/notes/:id", (req, res) => {
 
-    console.log(notes[1].id);
+    fs.readFile('db/db.json', (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
 
-    //res.json({ ok: true });
+      let newNote = []
+      for (let i = 0; i < notes.length; i++) {
+
+        if (notes[i].id != req.params.id) {
+          newNote.push(notes[i])
+        }
+      }
+
+      fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+        if (err) {
+          console.log(err);
+        }
+        res.json({ ok: true });
+      });
+
+
+    });
+
 
   });
 
